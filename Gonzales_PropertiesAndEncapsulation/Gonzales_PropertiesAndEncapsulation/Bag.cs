0ff1b6552eaa9110f0c339bacc0cf32059557598
ACT-STUDENT <ACT-STUDENT@LAB3-PC10>
2025-08-27 14:33:03 +0800

    Program on basic encapsulation

---
+++ Gonzales_PropertiesAndEncapsulation/Gonzales_PropertiesAndEncapsulation/Bag.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gonzales_PropertiesAndEncapsulation
+{
+    internal class Bag
+    {
+        //Declare the properties of the Bag class
+        private string brand;
+        private string color;
+        private int capacity;
+
+        //Set the properties for the Bag class fields
+        public string Brand
+        {
+            get { return brand; }
+            set { brand = value; }
+        }
+        public string Color
+        {
+            get { return color; }
+            set { color = value; }
+        }
+        public int Capacity
+        {
+            get { return capacity; }
+            set { capacity = value; }
+        }
+        //Method to display bag information
+        public void DisplayBagInfo()
+        {
+            Console.WriteLine("\t Here are the Bag Details");
+            Console.WriteLine($"\n \t Brand: {Brand}\n \t Color: {Color}\n \t Capacity: {Capacity}\n");
+        }
+    }
+}
---
+++ Gonzales_PropertiesAndEncapsulation/Gonzales_PropertiesAndEncapsulation/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gonzales_PropertiesAndEncapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiate the Bag class
+            Bag myBag = new Bag();
+
+            //Set the properties of the Bag class fields
+            myBag.Brand = "Hermes";
+            myBag.Color = "Pink";
+            myBag.Capacity = 5; // medium-sized Hermès bag capacity in liters
+
+            //Display the Bag Information
+            myBag.DisplayBagInfo();
+
+            Console.ReadKey();
+        }
+    }
+}
