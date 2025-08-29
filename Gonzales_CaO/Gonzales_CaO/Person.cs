055c721c2128776203bea1d26106612f4fb833cc
ACT-STUDENT <ACT-STUDENT@LAB3-PC10>
2025-08-29 14:21:11 +0800

    Progam on Creating a Class and Object

---
+++ Gonzales_CaO/Gonzales_CaO/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gonzales_CaO
+{
+    internal class Person
+    {
+        //Class members
+        public string name;
+        public int age;
+        public string gender;
+
+        //Method Display
+        public void Greet()
+        {
+            Console.WriteLine("Annyeohaseyo! My name is " + name + " and I am " + age + " years of age, and I'm a " + gender + ".");
+        }
+    }
+        
+}
---
+++ Gonzales_CaO/Gonzales_CaO/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gonzales_CaO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person(); //instantiation of an object
+
+            //instantiation of fields
+            person1.name = "Shanly Anne";
+            person1.age = 19;
+            person1.gender = "Female";
+
+            //call the method
+            person1.Greet();
+
+            Console.ReadKey();
+        }
+    }
+}
