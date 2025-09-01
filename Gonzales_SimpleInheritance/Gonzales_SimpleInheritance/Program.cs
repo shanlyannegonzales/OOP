7cdb4fc008bfd5bb5cece7885a8bb8710bd65341
ACT-STUDENT <ACT-STUDENT@LAB3-PC10>
2025-09-01 14:54:54 +0800

    Program on Simple Inheritance

---
+++ Gonzales_SimpleInheritance/Gonzales_SimpleInheritance/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gonzales_SimpleInheritance
+{
+    internal class Program
+    {
+        //Name: Gonzales, Shanly Anne
+        //Section: IT306
+        //Exercise Title: Use these classes:  Student class (Base class); RegularStudent class (Derived class); IrregularStudent class (Derived class)
+        static void Main(string[] args)
+        {
+            //Create an instance of the Student class
+            Student mystudent = new Student
+            {
+                Name = "Darren Paul Macuto",
+                Program = "BSIT"
+            };
+            mystudent.StudentBasicInfo(); //Call method to display student info
+
+            //Create an instance of the Regular student
+            Regular myRegular = new Regular
+            {
+                Name = "Shanly Anne Gonzales",
+                Program = "BSIT",
+                Section = "IT306"
+            };
+            myRegular.StudentBasicInfo(); //Call inherited method to display Student info
+            myRegular.SectionEnrolled(); //Call inherited method to display Regular info
+
+            //Create an instance of the Irregular class
+            Irregular myIrregular = new Irregular
+            {
+                Name = " Ryan Salvo",
+                Program = "BSHM",
+                UnitsEnrolled = "Irregular"
+            };
+            myIrregular.StudentBasicInfo(); //Call inherited method to display Student info
+            myIrregular.EnrolledSemUnits();  //Call inherited method to display Irregular info
+
+            //Wait for user input before closing
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
+
+
+          
---
+++ Gonzales_SimpleInheritance/Gonzales_SimpleInheritance/Student.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using static System.Collections.Specialized.BitVector32;
+
+namespace Gonzales_SimpleInheritance
+{
+    //Name: Gonzales, Shanly Anne
+    //Section: IT306
+    //Exercise Title: Use these classes:  Student class (Base class); RegularStudent class (Derived class); IrregularStudent class (Derived class)
+    class Student //Base class
+    {
+        //Prooperties for the Student Class
+        public string Name { get; set; }
+        public string Program { get; set; }
+
+        //Method to display student information
+        public void StudentBasicInfo()
+        {
+            Console.WriteLine("\n ----- Student Information -----\n");
+            Console.WriteLine($"Name: {Name}, \nProgram: {Program}");
+        }
+    }
+
+    class Regular : Student //Derived class
+    {
+        //Additional property for the Regular class
+        public string Section { get; set; }
+
+        //Method to display regular information
+        public void SectionEnrolled()
+        {
+            Console.WriteLine($"Section: {Section}");
+        }
+    }
+    class Irregular : Student //Another derived class
+    {
+        //Additional property for the Irregular class
+        public string UnitsEnrolled { get; set; }
+
+        //Method to display Irregular information
+        public void EnrolledSemUnits()
+        {
+            Console.WriteLine($"UnitsEnrolled: {UnitsEnrolled}");
+        }
+    }
+}
+
+
+
+
+//Use these classes:  Student class (Base class); RegularStudent class (Derived class); IrregularStudent class (Derived class)
+//Student class properties : Name and Program
+//RegularStudent property: Section
+//IrregularStudent property: UnitsEnrolled
+//Student class method : BasicInfo()
+//RegularStudent method: SectionEnrolled()
+//IrregularStudent method: EnrolledSemUnits()
+//In the program class, instantiate an object for each derived class, then display the student information for both objects, which includes the Name and Program.
+
+//Name: Gonzales, Shanly Anne
+//Section: IT306
+//Exercise Title: Use these classes:  Student class (Base class); RegularStudent class (Derived class); IrregularStudent class (Derived class)
