a5135d1c2797b05dc8689196db8d93be8ee94538
ACT-STUDENT <ACT-STUDENT@LAB4-PC8>
2025-09-04 14:14:22 +0800

    Program on Simple Inheritance with Constructor

---
+++ Gonzales_SimpleInheritanceWithConstructor/Gonzales_SimpleInheritanceWithConstructor/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gonzales_SimpleInheritanceWithConstructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" ----These are the student details---- ");
+            Regular regular = new Regular("Shanly Anne Gonzales", "BSIT", "IT306"); // Create Student Info
+            regular.SectionEnrolled();  // Display Section Info
+
+            Irregular irregular = new Irregular("Darren Paul Macuto", "HM102", "28");
+            irregular.EnrolledSemUnits();  //Display Units Info
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Gonzales_SimpleInheritanceWithConstructor/Gonzales_SimpleInheritanceWithConstructor/Student.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gonzales_SimpleInheritanceWithConstructor
+{
+    class Student // Base Class
+    {
+        //Fields
+        public string name;
+        public string program;
+
+        public Student(string studentName, string studentProgram)
+        {
+            name = studentName;
+            program = studentProgram;
+        }
+
+        // Constructor
+        public void BasicInfo()
+        {
+            Console.WriteLine($"\nName: {name}, \nProgram: {program}");
+        }
+    }
+
+    class Regular : Student // Derived class
+    {
+        // Field
+        public string Section;
+
+        // Constructor
+        public Regular(string studentName, string studentProgram, string studentSection) : base(studentName, studentProgram)
+        {
+            Section = studentSection;
+        }
+
+        // Method to Display Section Info
+        public void SectionEnrolled()
+        {
+            BasicInfo(); // Call base class method
+            Console.WriteLine($"Section: {Section}");
+        }
+    }
+
+    class Irregular : Student // Derived class
+    {
+        public string UnitsEnrolled;
+
+        // Constructor
+        public Irregular(string studentName, string studentProgram, string studentUnitsEnrolled) : base(studentName, studentProgram)
+        {
+            UnitsEnrolled = studentUnitsEnrolled;
+        }
+
+        // Method to Display Units Info
+        public void EnrolledSemUnits()
+        {
+            BasicInfo();
+            Console.WriteLine($"UnitsEnrolled: {UnitsEnrolled}");
+        }
+    }
+}
